cc0769102e261d06dbb14471ac4e8300a5371607
Patrice Allardin <patrice.allardin@free.fr>
2019-05-16 17:14:52 +0200

    tppizzav1.1

---
+++ src/main/java/dev/pizzeria/model/Commande.java
@@ -0,0 +1,16 @@
+package dev.pizzeria.model;
+
+import java.util.Date;
+
+public class Commande {
+	private int id;
+	private int nuumero;
+	private Date date;
+	private String client;
+	private String livreur;
+
+	public Commande() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
